88f5ba18df84e0ffe81a1c556a131a9959db2ca5
Volkan <Volkan.05888@gmail.com>
2025-08-14 17:49:26 +0300

    Base ConnectionString

--- KayraWebAPI/ApiServiceRegistration.cs
+++ KayraWebAPI/ApiServiceRegistration.cs
@@ -1,12 +1,19 @@
-﻿using KayraWebAPI.Repository;
+﻿using KayraWebAPI.Context;
+using KayraWebAPI.Repository;
 using KayraWebAPI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace KayraWebAPI
 {
     public static class ApiServiceRegistration
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<BaseDbContext>(options =>
+            options.UseSqlServer(
+                                                            configuration.GetConnectionString("kayraProjectConnectionString")),
+                                                            ServiceLifetime.Scoped);
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IProductService, ProductManager>();
             return services;
---
+++ KayraWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,105 @@
+﻿using KayraWebAPI.Dtos;
+using KayraWebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KayraWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GetProductDto>>> GetProduct()
+        {
+            try
+            {
+                var products=await _productService.GetAllProductsAsync();
+                return Ok(products);
+            }
+            catch (Exception)
+            {
+
+               return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while retrieving products.");
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetProductDto>> GetProductById(Guid id)
+        {
+            try
+            {
+                var product = await _productService.GetProductByIdAsync(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return Ok(product);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while retrieving the product.");
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<GetProductDto>> CreateProduct(CreateProductDto createProductDto)
+        {
+            try
+            {
+                if (createProductDto == null)
+                {
+                    return BadRequest("Invalid product data.");
+                }
+                var createdProduct = await _productService.CreateProductAsync(createProductDto);
+                return Ok(createdProduct);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while creating the product.");
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult<UpdateProductDto>> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            try
+            {
+                if (updateProductDto == null)
+                {
+                    return BadRequest("Invalid product data.");
+                }
+                var updatedProduct = await _productService.UpdateProductAsync(updateProductDto);
+                if (updatedProduct == null)
+                {
+                    return NotFound();
+                }
+                return Ok(updatedProduct);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while updating the product.");
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteProduct(Guid id, bool permanent = false)
+        {
+            try
+            {
+                var result = await _productService.DeleteProductAsync(id, permanent);
+                if (!result)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while deleting the product.");
+            }
+        }
+    }
+}
