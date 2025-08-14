b9493c3fc7efd73e9731f1c41144e3934a69a3fd
Volkan <Volkan.05888@gmail.com>
2025-08-14 17:49:48 +0300

    Fixed Programcs an error

--- KayraWebAPI/Program.cs
+++ KayraWebAPI/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddApiServices();
+builder.Services.AddApiServices(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
